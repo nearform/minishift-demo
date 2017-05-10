81bea5f3d8947ac35af0caae8f3d9180a57f554b
Dara Hayes <dara.hayes@nearform.com>
2017-05-10 15:46:40 +0100

    add incremental build support to node image | cache dependencies between builds

--- bin/demo-run.sh
+++ bin/demo-run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-set -e
+# set -e
 
 MINISHIFT_DIRECTORY="$(dirname $PWD)"
 PROJECT=minishift-demo
 
 OPENSHIFT_TOKEN=$(oc whoami -t)
-OPENSHIFT_REGISTRY="$(minishift openshift registry)"
 
 echo "Creating demo application..."
 echo "=========================="
@@ -14,19 +13,15 @@ echo "(1/5) Creating new project..."
 oc new-project $PROJECT --display-name="Demo Project" --description="A demo project designed to help you start developing with minishift" > /dev/null
 
 echo "=========================="
-echo "(2/5) Create builder image..."
-echo "Setting Docker daemon..."
-eval "$(minishift docker-env --shell bash)"
-echo "Login to Openshift registry..."
-docker login -u developer -p $OPENSHIFT_TOKEN $OPENSHIFT_REGISTRY > /dev/null
-echo "Build S2I image..."
-docker build -q $MINISHIFT_DIRECTORY/s2i-images/node-7.10.0 -t nearform/node:7.10.0
-echo "Tag S2I Image"
-docker tag nearform/node:7.10.0 $OPENSHIFT_REGISTRY/$PROJECT/node:7.10.0
-docker tag nearform/node:7.10.0 $OPENSHIFT_REGISTRY/$PROJECT/node:latest # Push with tag latest
-echo "Push to Openshift registry..."
-docker push $OPENSHIFT_REGISTRY/$PROJECT/node:7.10.0
-docker push $OPENSHIFT_REGISTRY/$PROJECT/node:latest
+
+if oc get builds node > /dev/null; then
+  echo "(2/5) node-6-builder exists. Skipping..."
+else
+  echo "(2/5) Creating node-6-builder image"
+  oc create -f ../demo/manifests/node-s2i.yml
+  oc start-build node --from-dir ../s2i-images/node-7.10.0 --follow
+fi
+
 
 echo "=========================="
 echo "(3/5) Deploying Mosquitto..."
