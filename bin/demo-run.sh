d5835b883878fa52d07a38de3b713c97f8dbf7cc
Dara Hayes <dara.hayes@nearform.com>
2017-05-10 16:18:29 +0100

    minor fixes

--- bin/demo-run.sh
+++ bin/demo-run.sh
@@ -2,6 +2,7 @@
 
 # set -e
 
+DIR=$(cd `dirname $0` && pwd)
 MINISHIFT_DIRECTORY="$(dirname $PWD)"
 PROJECT=minishift-demo
 
@@ -14,31 +15,32 @@ oc new-project $PROJECT --display-name="Demo Project" --description="A demo proj
 
 echo "=========================="
 
-if oc get builds node > /dev/null; then
-  echo "(2/5) node-6-builder exists. Skipping..."
+if oc get imagestream node > /dev/null; then
+  echo "(2/5) node builder image exists. Skipping..."
 else
-  echo "(2/5) Creating node-6-builder image"
-  oc create -f ../demo/manifests/node-s2i.yml
-  oc start-build node --from-dir ../s2i-images/node-7.10.0 --follow
+  echo "(2/5) Creating node builder image"
+  oc create -f $DIR/../demo/manifests/node-s2i.yml
+  oc start-build node --from-dir $DIR/../s2i-images/node-7.10.0 --follow
+  sleep 5
 fi
 
 
 echo "=========================="
 echo "(3/5) Deploying Mosquitto..."
-oc create -f $MINISHIFT_DIRECTORY/demo/manifests/mosquitto.yaml > /dev/null
+oc create -f $DIR/../demo/manifests/mosquitto.yaml > /dev/null
 
 sleep 5
 
 echo "=========================="
 echo "(4/5) Deploying server..."
-oc process -f $MINISHIFT_DIRECTORY/demo/manifests/nodejs.yaml -p NAME=server -p PROBE=/healthz | oc create -f - 
+oc process -f $DIR/../demo/manifests/nodejs.yaml -p NAME=server -p PROBE=/healthz | oc create -f - 
 oc set env dc/server SERVER_PORT=8080 LOG_LEVEL=debug MQTT_BROKER=mqtt://eclipse-mosquitto
-oc start-build server --from-dir $MINISHIFT_DIRECTORY/demo/server
+oc start-build server --from-dir $DIR/../demo/server
 
 sleep 5
 
 echo "=========================="
 echo "(5/5) Deploying client..."
-oc process -f $MINISHIFT_DIRECTORY/demo/manifests/nodejs.yaml -p NAME=client -p PROBE=/healthz | oc create -f -
+oc process -f $DIR/../demo/manifests/nodejs.yaml -p NAME=client -p PROBE=/healthz | oc create -f -
 oc set env dc/client SERVER_PORT=8080 LOG_LEVEL=debug MQTT_BROKER=mqtt://eclipse-mosquitto
-oc start-build client --from-dir $MINISHIFT_DIRECTORY/demo/client
+oc start-build client --from-dir $DIR/../demo/client
