462162ebd6c9e4b6e891eff761d09cf24ae4106d
Dara Hayes <dara.hayes@nearform.com>
2017-05-16 11:31:10 +0100

    Implementation where node_modules are installed in the container (#9)
    * implementation where node_modules are installed in the container (for native deps)
    
    * mount an empty volume on top of the standard node_modules folder + use standard node base image
    
    * added endpoint to test leveldb && npm script to start build in openshift
    
    * rename to hello-server in package.json

--- bin/demo-run.sh
+++ bin/demo-run.sh
@@ -12,12 +12,7 @@ echo "(1/4) Creating new project..."
 oc new-project $PROJECT --display-name="Demo Project" --description="A demo project designed to help you start developing with Minishift" > /dev/null
 
 echo "=========================="
-echo "(2/4) Installing node_modules..."
-
-cd "$MINISHIFT_DIRECTORY"/../demo/hello && npm install --quiet && cd - || exit 1
-
-echo "=========================="
-echo "(3/4) Deploying hello-server..."
+echo "(2/3) Deploying hello-server..."
 
 oc process -f "$MINISHIFT_DIRECTORY"/../demo/manifests/openshift/nodejs.yaml \
   -p NAMESPACE=$PROJECT \
@@ -27,5 +22,5 @@ oc process -f "$MINISHIFT_DIRECTORY"/../demo/manifests/openshift/nodejs.yaml \
   -p LOG_LEVEL=debug \
   -p APP_VOLUME="${MINISHIFT_DIRECTORY}"/../demo/hello | oc create -f -
 
-echo "(4/4) Building hello-server Image"
+echo "(3/3) Building hello-server Image"
 oc start-build $APP_NAME --from-dir $MINISHIFT_DIRECTORY/../demo/hello
--- demo/hello/index.js
+++ demo/hello/index.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const restify = require('restify')
+const level = require('level')
+
+var db = level('./testdb')
 
 const pino = require('pino')({
   level: process.env.LOG_LEVEL || 'debug',
@@ -19,6 +22,19 @@ server.get('/version', function (req, res, next){
   res.json({ version: '1.1' })
   
   return next()
+})
+
+server.get('/time', function(req, res, next) {
+  var t = new Date()
+  db.put('time', t, function (err) {
+    if (err) {
+      res.status(500)
+      res.json({msg: 'An error occurred'})
+      return next(err)
+    }
+    res.json({msg: t})
+    return next()
+  })
 })
 
 server.listen(process.env.SERVER_PORT || 5000, function () { // bind server to port 5000.
