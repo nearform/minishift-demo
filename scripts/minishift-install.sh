4691a9225e029b331ac67969534b3a596f240ce8
ron litzenberger <ron.litzenberger@gmail.com>
2017-05-23 22:38:29 -0500

    set max cpus to 5

--- scripts/minishift-install.sh
+++ scripts/minishift-install.sh
@@ -14,7 +14,7 @@ extract () {
 
 OS_TYPE=$(uname -s)
 
-echo "(1/5) Installing minishift..."
+echo "(1/6) Installing minishift..."
 LATEST_RELEASE=$(latest_release https://github.com/minishift/minishift/releases/latest)
 FILE=minishift.tgz
 DIR=./minishift
@@ -22,7 +22,7 @@ DIR=./minishift
 curl -sL https://github.com/minishift/minishift/releases/download/"${LATEST_RELEASE}"/minishift-"${LATEST_RELEASE:1}"-"${OS_TYPE}"-amd64.tgz -o $FILE
 extract $FILE $DIR
 
-echo "(2/5) Where do you want to copy the executable? (default: /usr/local/bin)"
+echo "(2/6) Where do you want to copy the executable? (default: /usr/local/bin)"
 read -r MINISHIFT_PATH
 
 if [[ -z $MINISHIFT_PATH ]]; then
@@ -32,7 +32,7 @@ fi
 cp $DIR/minishift $MINISHIFT_PATH
 rm -rf $FILE $DIR
 
-echo "(3/5) Which hypervisor do you want to use? (recommended: virtualbox)?"
+echo "(3/6) Which hypervisor do you want to use? (recommended: virtualbox)?"
 [[ "$OS_TYPE" == "Darwin" ]] && echo "(xhyve, virtualbox, vmware-fusion)" || echo "(kvm, virtualbox)"
 read -r HYPERVISOR
 
@@ -40,9 +40,28 @@ if [[ -z $HYPERVISOR ]]; then
   HYPERVISOR=virtualbox
 fi
 
-echo "(4/5) set minishift config.  Config found ./minishift/config/config.json"
+echo "(4/6) Is this a large node ( 50 pods )? (no, yes)"
+
+read -r SIZE
+
+if [[ -z $SIZE ]]; then
+  MEMORY=4096
+  CPUS=2
+elif [[ "$SIZE" = "no" ]]; then
+	MEMORY=4096
+  	CPUS=2
+elif [[ "$SIZE" = "yes" ]]; then
+	MEMORY=4096
+  	CPUS=5
+else 
+	MEMORY=4096
+  	CPUS=2 
+fi
+
+echo "(5/6) set minishift config.  View config at ./minishift/config/config.json"
 minishift config set vm-driver $HYPERVISOR
-minishift config set memory 4096
-minishift config set cpus 5
-echo "(5/5) minishift start"
+minishift config set memory $MEMORY
+minishift config set cpus $CPUS
+
+echo "(6/6) minishift start"
 minishift start
