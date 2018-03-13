d01a68d3cf6d7c47139b3b8aa47fd20cfff31ca9
john o'keeffe <jokeeffe09@gmail.com>
2018-03-13 17:09:14 +0000

    Changed hello-server to port 8080

--- hello-server/index.js
+++ hello-server/index.js
@@ -42,6 +42,6 @@ server.get('/time', function(req, res, next) {
   })
 })
 
-server.listen(process.env.SERVER_PORT || 5000, function () { // bind server to port 5000.
+server.listen(process.env.SERVER_PORT || 8080, function () { // bind server to port 5000.
   pino.info('%s listening at %s', server.name, server.url);
 });
